c49bb693b7005b19f353509ff6dc5287cbd74d66
torgerbocianowski <bocianot@hu-berlin.de>
2023-01-17 11:23:44 +0100

    error handling

--- pages/info_page.py
+++ pages/info_page.py
@@ -39,80 +39,81 @@ tabs_list = [
     'Cash Flow',
     'Earnings',
 ]
-# try:
-stock = st.selectbox('Select a company', tickers['Both'], key='info select')
-stock = stock.split(' ')[0]
+try:
+    stock = st.selectbox('Select a company', tickers['Both'], key='info select')
+    stock = stock.split(' ')[0]
 
-ticker = yf.Ticker(stock)
-tabs = st.tabs(tabs_list)
+    ticker = yf.Ticker(stock)
+    tabs = st.tabs(tabs_list)
 
-msg = 'Load Balance Sheet, Cash Flow Statement and Earnings'
-    
+    msg = 'Load Balance Sheet, Cash Flow Statement and Earnings'
+        
 
-with st.spinner('Please wait, this might take a few seconds...'):
-    with tabs[0]:
-        st.subheader('Balance Sheet')
-        df = ticker.balance_sheet
-        df.columns = [date.strftime("%d/%m/%Y") for date in df.columns]
-        df = df.fillna(0)
-        styled = df.style.format("{:,}")
-        st.dataframe(styled, use_container_width=True)
+    with st.spinner('Please wait, this might take a few seconds...'):
+        with tabs[0]:
+            st.subheader('Balance Sheet')
+            df = ticker.balance_sheet
+            st.write(df)
+            df.columns = [date.strftime("%d/%m/%Y") for date in df.columns]
+            df = df.fillna(0)
+            styled = df.style.format("{:,}")
+            st.dataframe(styled, use_container_width=True)
 
-    with tabs[1]:
-        st.subheader('Cash Flow')
-        df = ticker.cashflow
-        df.columns = [date.strftime("%d/%m/%Y") for date in df.columns]
-        df = df.fillna(0)
-        styled = df.style.format("{:,}")
-        st.dataframe(styled, use_container_width=True)
+        with tabs[1]:
+            st.subheader('Cash Flow')
+            df = ticker.cashflow
+            df.columns = [date.strftime("%d/%m/%Y") for date in df.columns]
+            df = df.fillna(0)
+            styled = df.style.format("{:,}")
+            st.dataframe(styled, use_container_width=True)
 
-    with tabs[2]:
-        st.subheader('Earnings')
-        df = ticker.earnings
-        df = df.fillna(0)
-        styled = df.style.format("{:,}")
-        col1, col2 = st.columns(2)
-        col1.dataframe(styled, use_container_width=True)
-        col2.bar_chart(df, use_container_width=True)
-    
+        with tabs[2]:
+            st.subheader('Earnings')
+            df = ticker.earnings
+            df = df.fillna(0)
+            styled = df.style.format("{:,}")
+            col1, col2 = st.columns(2)
+            col1.dataframe(styled, use_container_width=True)
+            col2.bar_chart(df, use_container_width=True)
+        
 
-    # reset = df.reset_index()
-    # print(reset)
-    # print(reset['Year'])
-    # fig = plt.figure(figsize=(8, 6))
-    # sns.barplot(data=reset, x='Revenue', y='Earnings', hue='Year')
-    # col2.pyplot(fig)
-
-
-with st.spinner(f'Loading Information for {stock}'):
-    if selected_page != 'Info':
-        st.stop()
-    else:
-        infos = [
-                'ebitda', 'grossProfits', 'freeCashflow', 'operatingCashflow', 
-                'totalCash', 'currentRatio', 'targetMeanPrice', 'debtToEquity'
-                ]
-
-        vals = [ticker.info[info] for info in infos]
-        vals = np.array([vals, np.ones(len(vals))])
-
-        info_df = pd.DataFrame(vals, columns=infos)
-
-        st.subheader(f'More Info about {stock}')
-        col1, col2 = st.columns(2)
-        col1.table(
-            info_df[:1].T
-            .iloc[: int(len(info_df.T)/2)].style.format("{:,}")
-        )
-        col2.table(
-            info_df[:1].T
-            .iloc[int(len(info_df.T)/2):].style.format("{:,}")
-        )
-        st.subheader(f'\nAbout {stock}')
-        st.write(ticker.info['longBusinessSummary'])
+        # reset = df.reset_index()
+        # print(reset)
+        # print(reset['Year'])
+        # fig = plt.figure(figsize=(8, 6))
+        # sns.barplot(data=reset, x='Revenue', y='Earnings', hue='Year')
+        # col2.pyplot(fig)
 
 
-# except:
-#     st.text(f'There was an issue loading the data for {stock}')
+    with st.spinner(f'Loading Information for {stock}'):
+        if selected_page != 'Info':
+            st.stop()
+        else:
+            infos = [
+                    'ebitda', 'grossProfits', 'freeCashflow', 'operatingCashflow', 
+                    'totalCash', 'currentRatio', 'targetMeanPrice', 'debtToEquity'
+                    ]
+
+            vals = [ticker.info[info] for info in infos]
+            vals = np.array([vals, np.ones(len(vals))])
+
+            info_df = pd.DataFrame(vals, columns=infos)
+
+            st.subheader(f'More Info about {stock}')
+            col1, col2 = st.columns(2)
+            col1.table(
+                info_df[:1].T
+                .iloc[: int(len(info_df.T)/2)].style.format("{:,}")
+            )
+            col2.table(
+                info_df[:1].T
+                .iloc[int(len(info_df.T)/2):].style.format("{:,}")
+            )
+            st.subheader(f'\nAbout {stock}')
+            st.write(ticker.info['longBusinessSummary'])
+
+
+except:
+    st.text(f'There was an issue loading the data for {stock}')
 
 
